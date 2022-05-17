41d40e5f8d326e38e61cee827e4fc5a2d671e60a
edward <bepyan@naver.com>
2022-05-17 20:29:31 +0900

    refactor. main _middleware

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,24 +2,25 @@ import type { NextFetchEvent, NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 const RESTRICT_ROUTES = ['/login', '/register'];
-const PUBLIC_ROUTES = [...RESTRICT_ROUTES];
 
 export const middleware = (req: NextRequest, ev: NextFetchEvent) => {
   if (req.ua?.isBot) {
     return new Response('미치셨습니까 휴먼?', { status: 403 });
   }
 
-  if (!req.url.includes('/api')) {
-    if (
-      req.cookies.mildvu_session &&
-      RESTRICT_ROUTES.some((route) => req.url.includes(route))
-    ) {
-      return NextResponse.redirect(new URL(`/me`, req.url));
-    } else if (
-      !req.cookies.mildvu_session &&
-      PUBLIC_ROUTES.every((route) => !req.url.includes(route))
-    ) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
+  const pagename = req.page.name;
+  const isAPI = req.url.includes('/api');
+  if (isAPI || !pagename) return;
+
+  const hasSession = !!req.cookies.mildvu_session;
+  const isRestrictRoute = RESTRICT_ROUTES.some((route) => route === pagename);
+  const isPrivateRoute = pagename.includes('/me');
+
+  if (hasSession && isRestrictRoute) {
+    return NextResponse.redirect(new URL(`/me`, req.url));
+  }
+
+  if (!hasSession && isPrivateRoute) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
 };
