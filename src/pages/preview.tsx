f8f13613d601f03bde112b732db19e38529206d8
edward <bepyan@naver.com>
2022-05-20 02:13:41 +0900

    feat. preview 캐러셀

---
+++ src/pages/preview.tsx
@@ -0,0 +1,49 @@
+import { Button, Layout } from '@components';
+import { useMemo, useState } from 'react';
+
+const imgs = [
+  'https://www.nemopan.com/files/attach/images/6294/386/211/014/a04168af65afb12afa1936a98d372e1d.jpeg',
+  'https://www.nemopan.com/files/attach/images/6294/386/211/014/f2b6641879ea73de2fac8c957b624740.jpeg',
+  'https://www.nemopan.com/files/attach/images/6294/386/211/014/542dea3664436d543c3f273cea26dbda.jpeg',
+];
+
+export default () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const transformStyle = useMemo(
+    () => ({
+      transform: `translateX(${-100 * (currentIndex + 1)}%)`,
+      transition: `transform 300ms ease-in-out 0s`,
+    }),
+    [currentIndex],
+  );
+
+  return (
+    <Layout title="preview" className="pt-16 ">
+      <div className=" text-center">
+        <h1 className="text-5xl font-bold text-purple-500">Mildvu</h1>
+      </div>
+
+      <div className="relative mt-16 h-[700px] w-full overflow-hidden">
+        <div className="absolute flex h-full w-full" style={transformStyle}>
+          {imgs.map((v, i) => {
+            return (
+              <div key={i} className="h-full w-full" data-index={i - 1}>
+                <div className="pointer-events-none w-[576px] select-none">
+                  <img src={v} alt="" className=" h-auto w-full " />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <Button className="mt-2" onClick={() => setCurrentIndex((v) => v - 1)}>
+        prev
+      </Button>
+      <Button className="mt-2" onClick={() => setCurrentIndex((v) => v + 1)}>
+        next
+      </Button>
+    </Layout>
+  );
+};
